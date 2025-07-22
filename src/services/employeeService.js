c905ced4245640d906f6a55e83f5a55521400ce7
vuquanghung247 <hung.vuquang@vti.com.vn>
2025-07-22 16:46:07 +0700

    Your commit message here

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,48 +1,54 @@
 import axios from "axios";
 
 export default class EmployeeService {
-    getAuthHeaders() {
-        const token = localStorage.getItem("accessToken");
-        return {
-            Authorization: `Bearer ${token}`,
-        };
-    }
+  getAuthHeaders() {
+    const token = localStorage.getItem("accessToken");
+    return {
+      Authorization: `Bearer ${token}`,
+    };
+  }
 
-    getAll(pageNumber = 1, pageSize = 10, searchData = {}) {
-        return axios.post(
-            `https://localhost:7000/api/Employee/get-all-employee`,
-            searchData,
-            {
-                headers: this.getAuthHeaders(),
-                params: {
-                    pageNumber,
-                    pageSize,
-                },
-            }
-        );
-    }
+  getAll(pageNumber = 1, pageSize = 10, searchData = {}) {
+    return axios.post(
+      `https://localhost:7000/api/Employee/get-all-employee`,
+      searchData,
+      {
+        headers: this.getAuthHeaders(),
+        params: {
+          pageNumber,
+          pageSize,
+        },
+      }
+    );
+  }
 
+  getById(id) {
+    return axios.get(
+      `https://localhost:7000/api/Employee/get-employee-by-id/${id}`,
+      {
+        headers: this.getAuthHeaders(),
+      }
+    );
+  }
 
-    getById(id) {
-        return axios.get(`https://localhost:7000/api/Employee/get-employee-by-id/${id}`, {
-            headers: this.getAuthHeaders(),
-        });
-    }
+  addEmployee(employeeData) {
+    return axios.post(
+      "https://localhost:7000/api/Employee/add-employee",
+      employeeData,
+      {
+        headers: this.getAuthHeaders(),
+      }
+    );
+  }
 
-    addEmployee(employeeData) {
-        return axios.post("https://localhost:7000/api/Employee/add-employee", employeeData, {
-            headers: this.getAuthHeaders()
-        });
-    }
-
-    updateEmployee(id, data) {
-        console.log("edit employee", data);
-        return axios.put(
-            `https://localhost:7000/api/Employee/update-employee/${id}`,
-            data,
-            {
-                headers: this.getAuthHeaders(),
-            }
-        );
-    }
+  updateEmployee(id, data) {
+    console.log("edit employee", data);
+    return axios.put(
+      `https://localhost:7000/api/Employee/update-employee/${id}`,
+      data,
+      {
+        headers: this.getAuthHeaders(),
+      }
+    );
+  }
 }
